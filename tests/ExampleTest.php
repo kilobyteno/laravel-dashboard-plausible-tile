9018362aefa46e2e47d1aefcb67bba1d1ab3d0fc
Daniel S. Billing <daniel@kilobyte.no>
2022-05-02 21:43:30 +0200

    Update tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Spatie\MyTile\Tests;
+namespace Kilobyteno\PlausibleTile\Tests;
 
 use PHPUnit\Framework\TestCase;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Kilobyteno\PlausibleTile\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
